6f8de59ce410df20b8f6ad6f35d82b8fff7e1485
Ciro <Ciro16@gmail.com>
2021-06-23 17:12:38 -0700

    updated db url

--- db.js
+++ db.js
@@ -1,17 +1,34 @@
 const { Client } = require("pg");
 
-let DB_URI;
-
-if (process.env.NODE_ENV === "test") {
-    DB_URI = "postgresql:///subchef_test";
-} else {
-    DB_URI = "postgresql:///subchef";
-}
-
-let db = new Client({
-    connectionString: DB_URI
+const db = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect();
+
+db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    db.end();
+});
+
+// let DB_URI;
+
+// if (process.env.NODE_ENV === "test") {
+//     DB_URI = "postgresql:///subchef_test";
+// } else {
+//     DB_URI = "postgresql:///subchef";
+
+
+// let db = new Client({
+//     connectionString: DB_URI
+// });
+
+// db.connect();
 
 module.exports = db;
